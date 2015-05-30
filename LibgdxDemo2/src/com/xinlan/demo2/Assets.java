34fba7e1099cf6b3e38926e85745525e0cf0ec57
siwangqishiq <525647740@qq.com>
2015-05-30 11:09:43 +0800

    https://github.com/siwangqishiq/LibgdxDemo2.git

--- LibgdxDemo2/src/com/xinlan/demo2/Assets.java
+++ LibgdxDemo2/src/com/xinlan/demo2/Assets.java
@@ -1,21 +1,42 @@
 package com.xinlan.demo2;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.assets.AssetDescriptor;
 import com.badlogic.gdx.assets.AssetErrorListener;
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.utils.Disposable;
 
 public class Assets implements Disposable, AssetErrorListener {
 	public static final String TAG = Assets.class.getName();
 	public static final Assets instance = new Assets();
+	private AssetManager assetManager;
+
+	private Assets() {
+	}
+
+	public void init(AssetManager assetManager) {
+		this.assetManager = assetManager;
+		// set asset manager error handler
+		assetManager.setErrorListener(this);
+		// load texture atlas
+		assetManager.load(Constants.TEXTURE_ATLAS_OBJECTS, TextureAtlas.class);
+		// start loading assets and wait until finished
+		assetManager.finishLoading();
+		Gdx.app.debug(TAG,
+				"# of assets loaded: " + assetManager.getAssetNames().size);
+		for (String a : assetManager.getAssetNames())
+			Gdx.app.debug(TAG, "asset: " + a);
+	}
 
 	@Override
 	public void error(AssetDescriptor asset, Throwable throwable) {
-
+		Gdx.app.error(TAG, "Couldn't load asset '"
+				+ Constants.TEXTURE_ATLAS_OBJECTS + "'", (Exception) throwable);
 	}
 
 	@Override
 	public void dispose() {
-
+		assetManager.dispose();
 	}
-
 }// end class
